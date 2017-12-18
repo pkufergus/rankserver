a0c4ad2978b47f9b4779fe9b2589fdb3c1a1925e
Cao Song <caosong@baidu.com>
2017-12-19 01:42:33 +0800

    add first interface
    Signed-off-by: Cao Song <caosong@baidu.com>

--- controllers/default.go
+++ controllers/default.go
@@ -13,3 +13,21 @@ func (c *MainController) Get() {
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
+
+type MenuController struct {
+	beego.Controller
+}
+type MenuResp struct {
+	Id       uint64
+	Name     string
+	FatherId uint64
+}
+
+func (c *MenuController) Get() {
+	m := MenuResp{}
+	m.Id = 1
+	m.Name = "car"
+	m.FatherId = 0
+	c.Data["json"] = &m
+	c.ServeJSON()
+}
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"github.com/pkufergus/rankserver/controllers"
 	"github.com/astaxie/beego"
+	"github.com/pkufergus/rankserver/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/menu", &controllers.MenuController{})
 }
